3637f67dc6c32205d6e0b688a2789d534125f454
Zara Cooper <zramcooper@gmail.com>
2021-05-29 22:22:10 +0300

    Add tour events component

---
+++ src/app/features/tour-events/tour-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-events',
+  templateUrl: './tour-events.component.html',
+  styleUrls: ['./tour-events.component.css']
+})
+export class TourEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
