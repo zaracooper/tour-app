dded8f3bf2ddc660b76abbe90a6d53795e15c5df
Zara Cooper <zramcooper@gmail.com>
2021-05-30 01:20:20 +0300

    Add imports to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FeaturesModule,
     AppRoutingModule
   ],
   providers: [],
