dc0592e889e7ada78091bcffe6030f70f2d85795
Zara Cooper <zramcooper@gmail.com>
2021-05-30 01:21:24 +0300

    Add method to get events in tour event service

--- src/app/core/services/tour-events.service.ts
+++ src/app/core/services/tour-events.service.ts
@@ -1,12 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { UntilDestroy } from '@ngneat/until-destroy';
+import { environment } from 'src/environments/environment';
+import { TourEvent } from '../models/tour-event';
 
-@UntilDestroy({ checkProperties: true })
 @Injectable({
   providedIn: 'root'
 })
 export class TourEventsService {
+  private eventsPath = 'tour-events';
 
   constructor(private http: HttpClient) { }
+
+  getEvents(){
+    return this.http.get<TourEvent[]>(environment.apiUrl+this.eventsPath);
+  }
 }
