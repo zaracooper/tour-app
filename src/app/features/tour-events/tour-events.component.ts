3918fe928255a13520266502a32e08057582cbac
Zara Cooper <zramcooper@gmail.com>
2021-05-30 01:24:55 +0300

    Add styling to tour events component Add content to tour events component Get events when tour event component initializes

--- src/app/features/tour-events/tour-events.component.ts
+++ src/app/features/tour-events/tour-events.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { TourEvent } from 'src/app/core/models/tour-event';
+import { TourEventsService } from 'src/app/core/services/tour-events.service';
 
+@UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-tour-events',
   templateUrl: './tour-events.component.html',
   styleUrls: ['./tour-events.component.css']
 })
 export class TourEventsComponent implements OnInit {
+  events: TourEvent[] = [];
 
-  constructor() { }
+  constructor(private eventService: TourEventsService) { }
 
   ngOnInit(): void {
+    this.eventService.getEvents().subscribe((events) => this.events = events);
   }
-
 }
