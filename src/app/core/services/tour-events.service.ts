364761c9f31f44884abb1f74388d4624d5e00b1f
Zara Cooper <zramcooper@gmail.com>
2021-05-29 22:50:40 +0300

    Add http imports

--- src/app/core/services/tour-events.service.ts
+++ src/app/core/services/tour-events.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
 
+@UntilDestroy({ checkProperties: true })
 @Injectable({
   providedIn: 'root'
 })
 export class TourEventsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
