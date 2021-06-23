4b4544190cb0b09d2adbf4d5c7de683f85836b47
Zara Cooper <zramcooper@gmail.com>
2021-06-23 15:09:41 +0300

    Remove until destroy

--- src/app/features/tour-events/tour-events.component.ts
+++ src/app/features/tour-events/tour-events.component.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { UntilDestroy } from '@ngneat/until-destroy';
-import { TourEvent } from 'src/app/core/models/tour-event';
+import { Component } from '@angular/core';
 import { TourEventsService } from 'src/app/core/services/tour-events.service';
 
-@UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-tour-events',
   templateUrl: './tour-events.component.html',
   styleUrls: ['./tour-events.component.css']
 })
-export class TourEventsComponent implements OnInit {
-  events: TourEvent[] = [];
+export class TourEventsComponent {
+  events$ = this.eventService.getEvents();
 
   constructor(private eventService: TourEventsService) { }
-
-  ngOnInit(): void {
-    this.eventService.getEvents().subscribe((events) => this.events = events);
-  }
 }
